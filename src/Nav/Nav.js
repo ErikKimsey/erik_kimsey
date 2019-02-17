519bf0c63a82e140c7ea56d7e887153728639fe0
Erik Kimsey <kimseyerik@gmail.com>
2019-02-17 07:59:40 -0500

    Nav is now a class

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -1,39 +1,43 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { Link } from 'react-router-dom';
 import Banner from '../Banner/Banner';
 import './nav.scss';
 
-const Nav = () => {
-  const toggleBtn = document.querySelector('.nav-list');
-  const handleMobileToggle = () => {
-    toggleBtn.classList.toggle('.active');
-    console.log(toggleBtn);
+class Nav extends Component {
+
+  componentDidMount = () => {
+    let navList = document.querySelector('.nav-list');
+    console.log(navList);
     
   }
-  return (
-    <div className="nav-container">
-      <div className="toggle-logo-container">
-          <Banner />
-        <span class="navbar-toggle" id="js-navbar-toggle" onClick={handleMobileToggle}>
-            <i class="fas fa-bars"></i>
-      </span>
+  
+
+  render(){
+    return (
+      <div className="nav-container">
+        <div className="toggle-logo-container">
+            <Banner />
+          <span className="navbar-toggle" id="js-navbar-toggle" >
+              <i className="fas fa-bars"></i>
+        </span>
+        </div>
+        <ul className="nav-list">
+          <li>
+            <Link className="nav-link" to="/">home</Link>
+          </li>
+          <li>
+            <Link className="nav-link" to="/about">me</Link>
+          </li>
+          <li>
+            <Link className="nav-link" to="/work">work</Link>
+          </li>
+          <li>
+            <Link className="nav-link" to="/contact">contact me</Link>
+          </li>
+        </ul>
       </div>
-      <ul className="nav-list">
-        <li>
-          <Link className="nav-link" to="/">home</Link>
-        </li>
-        <li>
-          <Link className="nav-link" to="/about">me</Link>
-        </li>
-        <li>
-          <Link className="nav-link" to="/work">work</Link>
-        </li>
-        <li>
-          <Link className="nav-link" to="/contact">contact me</Link>
-        </li>
-      </ul>
-    </div>
-  )
+      )
+  }
 }
 
 export default Nav;
