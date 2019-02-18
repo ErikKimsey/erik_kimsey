cf1c831cf45ff717571d305138705e694d5c473f
Erik Kimsey <kimseyerik@gmail.com>
2019-02-18 16:02:26 -0500

    More About content and styling

--- src/About/About.js
+++ src/About/About.js
@@ -4,15 +4,22 @@ import './about.scss';
 const About = () => {
   return (
     <div className="about-container">
-    <p>Software Chef</p>
-    <h5 className="fav-recipes">favorite recipes:</h5>
-    <ul>
-      <li>UI + beauty + interactivity</li>
-      <li>JS + Sass + Web APIs + React</li>
-      <li>mobile-device sensors + new Web APIs + art&sound</li>
-      <li>React + Express + Apollo + GraphQL</li>
-      <li></li>
-    </ul>
+    {/* <p>Software Chef</p>
+    <h5 className="fav-recipes">favorite recipes:</h5> */}
+      <p>UI + beauty + interactivity</p>
+    <div className="liked-things-container">
+      <p>:::::: ::::: :::: ::: :: :   JS + Sass + React + GreenSock</p>
+      <p>: :::: ::: :: :   React + Express + Apollo + GraphQL</p>
+      <p>::: :::: ::: :: :   ReactStrap + Illustrator + FontAwesome</p>
+      <p>: ::: :: :   mobile-device sensors + new Web APIs + art&sound</p>
+      <p>: ::::: :::: ::: :: :   Webpack + Yarn + GitHub</p>
+      <p>::: :: :   Python + Django</p>
+      <p>:: ::: :: :   P5.js + </p>
+      <p>: :   C++ + Juce + frustration</p>
+      <p>: ::: :: :   D3.js + Angular + German</p>
+      <p> :: :   Processing + Arduino + freetime + IC</p>
+      <p>: :: :   Java + Android + Android Studio</p>
+    </div>
     </div>
   )
 }
