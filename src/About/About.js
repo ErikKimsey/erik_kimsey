19afb22a9e0378756dd7163c884c626baa746949
Erik Kimsey <kimseyerik@gmail.com>
2019-02-18 15:36:26 -0500

    Added more content to About

--- src/About/About.js
+++ src/About/About.js
@@ -4,11 +4,15 @@ import './about.scss';
 const About = () => {
   return (
     <div className="about-container">
-    <p>One solid dude.</p>
-    <h5>software developer.</h5>
-      <h4>About</h4>
-    <p>full-stack web: JS & libraries / CSS & SASS / </p>
-    <p></p>
+    <p>Software Chef</p>
+    <h5 className="fav-recipes">favorite recipes:</h5>
+    <ul>
+      <li>UI + beauty + interactivity</li>
+      <li>JS + Sass + Web APIs + React</li>
+      <li>mobile-device sensors + new Web APIs + art&sound</li>
+      <li>React + Express + Apollo + GraphQL</li>
+      <li></li>
+    </ul>
     </div>
   )
 }
