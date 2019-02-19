24a76cfc2733fa92be55bf7ffe57d5aacb0a0cc6
Erik Kimsey <kimseyerik@gmail.com>
2019-02-19 09:58:14 -0500

    Adjusted size of and added inset shadow to routes-container in App.js. Created data for scrolling text in ME page

--- src/About/About.js
+++ src/About/About.js
@@ -4,21 +4,48 @@ import './about.scss';
 const About = () => {
   return (
     <div className="about-container">
-    {/* <p>Software Chef</p>
-    <h5 className="fav-recipes">favorite recipes:</h5> */}
+
+    <div className="self-blurb-container">
       <p>UI + beauty + interactivity</p>
+      <p>I don't much say "I love..." about much.  But, I love working creatively with technology. And I'm excited to learn, and playing with, any technology.</p>
+      <p>'Jack of All Trades' is a super-power. Not a hindrance or weakness.  I would wear that title as a badge of honor.</p>
+    </div>
+
+    <div className="tech-knowledge-container">
+      <div className="js-scroll"></div>
+      <div className="style-scroll"></div>
+      <div className="backend-scroll"></div>
+      <div className="lang-scroll"></div>
+      <div className="design-scroll"></div>
+    </div>
+
     <div className="liked-things-container">
-      <p>:::::: ::::: :::: ::: :: :   JS + Sass + React + GreenSock</p>
-      <p>: :::: ::: :: :   React + Express + Apollo + GraphQL</p>
-      <p>::: :::: ::: :: :   ReactStrap + Illustrator + FontAwesome</p>
-      <p>: ::: :: :   mobile-device sensors + new Web APIs + art&sound</p>
-      <p>: ::::: :::: ::: :: :   Webpack + Yarn + GitHub</p>
-      <p>::: :: :   Python + Django</p>
-      <p>:: ::: :: :   P5.js + </p>
-      <p>: :   C++ + Juce + frustration</p>
-      <p>: ::: :: :   D3.js + Angular + German</p>
-      <p> :: :   Processing + Arduino + freetime + IC</p>
-      <p>: :: :   Java + Android + Android Studio</p>
+    <p>:::::: ::::: :::: ::: :: :</p>
+      <p>JS + Sass + React + GreenSock</p>
+      <p>: :::: ::: :: : </p> 
+      <p> React + Express + Apollo + GraphQL</p>
+      <p>::: :::: ::: :: : </p> 
+      <p> ReactStrap + Illustrator + FontAwesome</p>
+      <p>: ::: :: : </p>  
+      <p>mobile-device sensors + new Web APIs + art&sound</p>
+      <p>: ::::: :::: ::: :: : </p> 
+      <p> Webpack + Yarn + GitHub</p>
+      <p>::: :: : </p>  
+      <p>Python + Django</p>
+      <p>:: ::: :: : </p>  
+      <p>P5.js + </p>
+      <p>: : </p>  
+      <p>C++ + Juce + frustration</p>
+      <p>: ::: :: : </p>  
+      <p>D3.js + Angular + German</p>
+      <p> :: : </p>  
+      <p>Processing + Arduino + freetime + IC</p>
+      <p>: :: : </p> 
+      <p> Java + Android + Android Studio</p>
+    </div>
+
+    <div className="education-container">
+
     </div>
     </div>
   )
---
+++ src/__data/data.js
@@ -0,0 +1,65 @@
+const JS_DATA = [
+  'JavaScript (ES6+)',
+  'React',
+  'P5.js',
+  'Node.js',
+  'Axios',
+  'Apollo'
+]
+
+const STYLE_DATA = [
+  'CSS3',
+  'SASS',
+  'Less',
+  'Style-Components',
+]
+
+const HTML_DATA = [
+  'HTML 5',
+  'Canvas',
+];
+
+const BACKEND_DATA = [
+  'SQL',
+  'SQL Lite',
+  'Node.js',
+  'Django',
+  'GraphQL',
+  'NoSQL',
+  'Redis',
+];
+
+const WEB_API_DATA = [
+  'Web Audio API',
+  'Accelerometer',
+  'Gryoscope',
+  'Orientation Sensor',
+  'Mic/Camera',
+  'Drag and Drop',
+  'Vibration API',
+]
+
+const LANGUAGE_DATA = [
+  'Python',
+  'C',
+  'Processing',
+  'Java (Android)',
+  'C++',
+]
+
+const DESIGN_DATA = [
+  'Illustrator',
+  'Photoshop',
+  'InDesign',
+  'XD',
+];
+
+export {
+  JS_DATA, 
+  BACKEND_DATA,
+  LANGUAGE_DATA,
+  HTML_DATA, 
+  STYLE_DATA, 
+  WEB_API_DATA,
+  DESIGN_DATA 
+}
