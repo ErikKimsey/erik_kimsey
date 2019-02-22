355bc8c8c3750c85d2d58464bc2edc632d5ed4c8
Erik Kimsey <kimseyerik@gmail.com>
2019-02-22 13:30:10 -0500

    Cleaned up data_modal.js

--- src/About/components/data_modal.js
+++ src/About/components/data_modal.js
@@ -5,36 +5,21 @@ import './styles/data_modal.scss';
 class DataModal extends Component {
   constructor(props){
     super(props);
-    console.log(props)
-    this.state = {
-      isActive: false,
-    }
-  }
-
-  handleToggle = () => {
-    // let modal = document.querySelector('.list-container');
-    // modal.classList.toggle('list-container-active');  
-    // console.log(modal);
-    // this.setState({isActive: !this.state.isActive});
   }
  
   render(){
     return (
       <div className="modal-container">
-      <h1 onClick={this.handleToggle}>{this.props.title}</h1>
-      <div className="list-container">
-        {
-      
-              this.props.data.map((item)=>{
-                return (
-                  <DataItem key={item} data={item}/>
-                )
-              })
-      
-        }
-      </div>
-
-
+        <h1>{this.props.title}</h1>
+        <div className="list-container">
+          {
+            this.props.data.map((item)=>{
+              return (
+                <DataItem key={item} data={item}/>
+              )
+            })
+          }
+        </div>
       </div>
     )
   }
