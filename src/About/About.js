a5f9cdc7921967cf339e91f797a15122eb514e37
Erik Kimsey <kimseyerik@gmail.com>
2019-02-25 15:45:30 -0500

    Added icons to About

--- src/About/About.js
+++ src/About/About.js
@@ -7,11 +7,31 @@ import SimpleSlider from './containers/Slider';
 import Education from './components/Education';
 import Employment from './components/Employment';
 
+// SVGs
+import adobeIMG from '../assets/icons/adobe.svg';
+import reactIMG from '../assets/icons/react.svg';
+import jsIMG from '../assets/icons/javascript.svg';
+import graphqlIMG from '../assets/icons/graphql.svg';
+import sassIMG from '../assets/icons/sass.svg';
+import pythonIMG from '../assets/icons/python.svg';
+import webpackIMG from '../assets/icons/webpack.svg';
+import bootstrap from '../assets/icons/bootstrap.svg';
+import illustratorIMG from '../assets/icons/adobeillustrator.svg';
+import photoshopIMG from '../assets/icons/adobephotoshop.svg';
+import cssIMG from '../assets/icons/css3.svg';
+import npmIMG from '../assets/icons/npm.svg';
+import html5IMG from '../assets/icons/html5.svg';
+import nodeIMG from '../assets/icons/node-dot-js.svg';
+import djangoIMG from '../assets/icons/django.svg';
+import d3IMG from '../assets/icons/d3-dot-js.svg';
+
+const SVGS = [adobeIMG, reactIMG, cssIMG, jsIMG, html5IMG, sassIMG, nodeIMG, npmIMG, pythonIMG, djangoIMG, d3IMG, webpackIMG,bootstrap, illustratorIMG, photoshopIMG, graphqlIMG];
+
 class About extends Component {
   constructor(props){
     super(props);
   }
-
+ 
   render(){
     return (
       <div className="about-container">
@@ -22,7 +42,14 @@ class About extends Component {
           <p>I like to think I'm at the cross-section of interactivity + aesthetic-appeal + innovation + good purpose.</p>
         </div>
   
+        <div className="icons-container">
+        {
+          SVGS.map((e)=>{
+            return <img className="icon" src={e} />
+          })
+        }
 
+        </div>
         <div className="tech-knowledge-container">
           <SimpleSlider className="slider" data={JS_DATA}/>
           <SimpleSlider className="slider" data={WEB_API_DATA}/>
