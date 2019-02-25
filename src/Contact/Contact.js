9e9a1ca103be4fb151a6669c3ee35814feecbc19
Erik Kimsey <kimseyerik@gmail.com>
2019-02-25 18:49:15 -0500

    Reworking Contact.js for copying email address to clipboard.  Unsuccessful, however.

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -1,16 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './contact.scss';
 import { CONTACT_DATA } from '../__data/data';
 
-const Contact = () => {
-  return (
-    <div className="contact-container">
-      <div className="name contact-item">{CONTACT_DATA.name}</div>
-      <div className="title contact-item">{CONTACT_DATA.role}</div>
-      <div className="email-address contact-item">{CONTACT_DATA.email}</div>
-      <div className="phone-number contact-item">{CONTACT_DATA.phone}</div>
-    </div>
-  )
+
+class Contact extends Component {
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    return (
+      <div className="contact-container">
+        <div className="name contact-item">{CONTACT_DATA.name}</div>
+        <div className="title contact-item">{CONTACT_DATA.role}</div>
+        <div className="email-address contact-item" id="email-address">{CONTACT_DATA.email}</div>
+        <div className="phone-number contact-item">{CONTACT_DATA.phone}</div>
+      </div>
+    )
+  }
 }
 
 export default Contact;
