7c498c2f2bccb068f1f772cf0a843b7e63729f4e
Erik Kimsey <kimseyerik@gmail.com>
2019-02-25 12:15:23 -0500

    Built-out Employment.js DOM and added classnames and tags to employment.scss.

--- src/About/components/Employment.js
+++ src/About/components/Employment.js
@@ -1,10 +1,33 @@
 import React from 'react';
 import './styles/employment.scss';
 
-export default function Employment() {
+export default function Employment(props) {
   return (
     <div className="employment-container">
-      
+      <div className="employer-name">{props.data.employer}</div>
+      <div className="role">{props.data.role}</div>
+      <div className="duration">{props.data.duration}</div>
+      <div className="role-description">{props.data.description}</div>
+      <div className="projects-container">
+        <ul>
+          {
+            props.data.projects.map((e,i)=>{
+              return <li>{e}</li>
+            })
+          }
+        </ul>
+      </div>
+      <div className="tech-container">
+      <ul>
+          {
+            props.data.tech.map((e,i)=>{
+              return <li>{e}</li>
+            })
+          }
+        </ul>
+      </div>
+      <div className=""></div>
+      <div className=""></div>
     </div>
   )
 }
