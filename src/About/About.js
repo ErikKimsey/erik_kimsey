6fe9538070a4a06bf291a8e4b910903c988b774e
Erik Kimsey <kimseyerik@gmail.com>
2019-02-26 09:39:10 -0500

    Added some new content to About

--- src/About/About.js
+++ src/About/About.js
@@ -39,7 +39,11 @@ class About extends Component {
         <div className="self-blurb-container">
           <p>I work creatively with technology.</p>
           <p>I focus on web-based and mobile-web technology, design, and content. </p>
+          <p>That said, I'm interested in work involving development of creative technology in the realm of mobile-web/PWA, responsive web,and IoT.</p>
           <p>I like to think I'm at the cross-section of interactivity + aesthetic-appeal + innovation + good purpose.</p>
+          <p>I love learning new languages and technology.  Especially, in order to put them to use in novel ways.</p>
+          <p>Proficient technologies include: </p>
+
         </div>
   
         <div className="icons-container">
