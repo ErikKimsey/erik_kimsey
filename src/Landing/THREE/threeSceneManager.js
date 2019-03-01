b82ffa1d8ffc661268e7658b0f2696820bf138e3
Erik Kimsey <kimseyerik@gmail.com>
2019-03-01 10:57:20 -0500

    Added threeInit file, threeSceneManager file and added ref to LAnding container for reference to canvas

---
+++ src/Landing/THREE/threeSceneManager.js
@@ -0,0 +1,26 @@
+import * as THREE from 'three';
+
+export const SceneManager = (container) => {
+
+
+  /**
+   * Variables/values defining:
+   * canvas dimensions,
+   * mouse x/y locations,
+   * ...
+   */
+
+
+  /**
+   * A Bunch of Methods:
+   */
+
+  // create Scene
+  // create Camera
+  // create Renderer
+  // event methods pertaining to:
+  // -- interactivity, 
+  // -- canvas resize, 
+  // -- update()
+
+}
