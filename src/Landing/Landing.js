45999ea74ab8ed2784b6ff53e690eaf498f91d0d
Erik Kimsey <kimseyerik@gmail.com>
2019-03-07 09:35:08 -0500

    Removed tracking code from Landing

--- src/Landing/Landing.js
+++ src/Landing/Landing.js
@@ -1,38 +1,21 @@
 import React, { Component } from 'react';
 import './landing.scss';
-import rhizome from '../assets/rhizome.png';
-const tracking = require('tracking');
 
 class Landing extends Component {
   constructor(props){
     super(props);
-    this.state = {
-      data:0,
-    }
+    this.state = {}
   }
 
   componentDidMount() {
-    var colors = new window.tracking.ColorTracker(['magenta', 'cyan', 'yellow']);
-    let img = document.querySelector('.image');
-    colors.on('track',(e)=>{
-      console.log(e.data);
-    });
 
-    window.tracking.track(img, colors);
-
-    navigator.geolocation.getCurrentPosition((position) => {
-      console.log(position);
-      
-      this.setState({data: position.coords.latitude});
-    });
   }
 
   
   render(){
     return (
       <div className="landing-container">
-        <img className="image" src={rhizome} />
-        {this.state.data}
+   
       </div>
     )
   }
