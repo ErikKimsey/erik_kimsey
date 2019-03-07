a4bfa49e536626e669c83730c56d63b71e30231c
Erik Kimsey <kimseyerik@gmail.com>
2019-03-07 09:36:18 -0500

    Cleaned up unnecessary code in sensor_play

--- src/Lil_Extra/sensor_play.js
+++ src/Lil_Extra/sensor_play.js
@@ -2,15 +2,4 @@
 
 export const sensorPlay = () => {
   console.log(window);
-  
-  // if ( 'AmbientLightSensor' in window ) {
-  //   const sensor = new AmbientLightSensor();
-  //   sensor.onreading = () => {
-  //     console.log('Current light level:', sensor.illuminance);
-  //   };
-  //   sensor.onerror = (event) => {
-  //     console.log(event.error.name, event.error.message);
-  //   };
-  //   sensor.start();
-  // }
 }
