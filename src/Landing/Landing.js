5fb05402308d5f49b8456390d48dc30d5cdd29df
Erik Kimsey <kimseyerik@gmail.com>
2019-03-08 14:10:12 -0500

    Started toying with D3.  Started Unburzt component.  Installed d3.

--- src/Landing/Landing.js
+++ src/Landing/Landing.js
@@ -1,22 +1,37 @@
 import React, { Component } from 'react';
 import './landing.scss';
+import Unburzt from './Unburzt/Unburzt';
 
 class Landing extends Component {
   constructor(props){
     super(props);
-    this.state = {}
+    this.state = {
+      width: 0,
+      height: 0,
+    }
   }
 
   componentDidMount() {
+    console.log(document.querySelector('.landing-container').offsetWidth);
+    window.addEventListener('resize', ()=>{ this.handleResize(); 
+    });
 
+    
   }
 
-  
+  // change dimensions of container in state
+  handleResize = () => {
+    this.setState({
+      width: document.querySelector('.landing-container').offsetWidth,
+      height: document.querySelector('.landing-container').offsetHeight 
+    })
+  }
+
   render(){
     return (
       <div className="landing-container">
         <div className="flesh-container">
-          
+          <Unburzt width={this.state.width} height={this.state.height} />
         </div>
       </div>
     )
---
+++ src/Landing/Unburzt/Unburzt.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+import d3 from 'd3'
+
+export default class Unburzt extends Component {
+
+  componentDidMount() {
+    console.log(this.props.height);
+    
+    var g = d3.select('svg')  // returns a handle to the <svg> element
+    .attr('width', this.props.width)  // sets the width of <svg> and then returns the <svg> element again
+    .attr('height', this.props.height)  // (same as width)
+    .append('g')  // adds a <g> element to the <svg> element. It returns the <g> element
+    .attr('transform', 'translate(' + width / 2 + ',' + height / 2 + ')');  // takes the <g> element and moves the [0,0] center over and down
+
+  }
+
+  render() {
+    return (
+      <div>
+        <svg></svg>
+      </div>
+    )
+  }
+}
