14b00008ec9e5c91a5ea3791f7bdbbb1a549268c
Erik Kimsey <kimseyerik@gmail.com>
2019-03-08 14:24:32 -0500

    Added test data

--- src/Landing/Unburzt/Unburzt.js
+++ src/Landing/Unburzt/Unburzt.js
@@ -6,11 +6,11 @@ export default class Unburzt extends Component {
   componentDidMount() {
     console.log(this.props.height);
     
-    var g = d3.select('svg')  // returns a handle to the <svg> element
-    .attr('width', this.props.width)  // sets the width of <svg> and then returns the <svg> element again
-    .attr('height', this.props.height)  // (same as width)
-    .append('g')  // adds a <g> element to the <svg> element. It returns the <g> element
-    .attr('transform', 'translate(' + this.props.width / 2 + ',' + this.props.height / 2 + ')');  // takes the <g> element and moves the [0,0] center over and down
+    var g = d3.select('svg')  
+    .attr('width', this.props.width)  
+    .attr('height', this.props.height) 
+    .append('g')  
+    .attr('transform', 'translate(' + this.props.width / 2 + ',' + this.props.height / 2 + ')'); 
 
   }
 
