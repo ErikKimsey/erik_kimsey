8e834e9a2a8ed5177f188fb6749ebac6f08b468e
Erik Kimsey <kimseyerik@gmail.com>
2019-03-11 14:15:51 -0400

    Self bllurb text is iterating on interval on Landing.js

--- src/Landing/Landing.js
+++ src/Landing/Landing.js
@@ -2,40 +2,43 @@ import React, { Component } from 'react';
 import './landing.scss';
 import Unburzt from './Unburzt/Unburzt';
 import { data } from './data';
-
+let i=0;
 class Landing extends Component {
   constructor(props){
     super(props);
     this.state = {
       width: 0,
       height: 0,
-
+      item:data[0],
     }
   }
 
   componentDidMount() {
-    
+    window.setInterval(()=>{
+      this.handleSelfSlide();
+    }, 2800);
   }
 
   handleSelfSlide = () => {
-    let i = 0, len = data.length, item = '';
-    if(i >= len) i = 0;
-    item = data[i];
-    i++;
-    return item;
+    let len = data.length, item = '';
+    if(i === len) {
+      i = 0;
+      item = data[i];
+    } else {
+      item = data[i];
+      i++;
+    }
+    console.log(item);
+    this.setState({ item: item });
   }
-
-
 
   render(){
     return (
       <div className="landing-container">
         <div className="something-cool-container"></div>
         <div className="self-blurb-container">
-          { 
-            window.setInterval(()=>{
-              this.handleSelfSlide();
-            })
+          {
+            this.state.item
           }
       </div>
       </div>
