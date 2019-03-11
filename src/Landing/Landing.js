e431049571b6038fc1ce2af4194acbf1e1d7e4de
Erik Kimsey <kimseyerik@gmail.com>
2019-03-11 14:03:54 -0400

    LAnding self blurb text

--- src/Landing/Landing.js
+++ src/Landing/Landing.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './landing.scss';
 import Unburzt from './Unburzt/Unburzt';
+import { data } from './data';
 
 class Landing extends Component {
   constructor(props){
@@ -8,19 +9,34 @@ class Landing extends Component {
     this.state = {
       width: 0,
       height: 0,
+
     }
   }
+
+  componentDidMount() {
+    
+  }
+
+  handleSelfSlide = () => {
+    let i = 0, len = data.length, item = '';
+    if(i >= len) i = 0;
+    item = data[i];
+    i++;
+    return item;
+  }
+
+
 
   render(){
     return (
       <div className="landing-container">
         <div className="something-cool-container"></div>
         <div className="self-blurb-container">
-          {/* <p>I work creatively with technology.</p> */}
-          <p>I focus on web-based and mobile-web technology, design, and content. </p>
-          {/* <p>That said, I'm interested in work involving development of creative technology in the realm of mobile-web/PWA, responsive web,and IoT.</p>
-          <p>I like to think I'm at the cross-section of interactivity + aesthetic-appeal + innovation + good purpose.</p>
-          <p>I love learning new languages and technology.  Especially, in order to put them to use in novel ways.</p> */}
+          { 
+            window.setInterval(()=>{
+              this.handleSelfSlide();
+            })
+          }
       </div>
       </div>
     )
