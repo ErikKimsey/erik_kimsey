1f280510ccc0aa386282b76806325b162494c94f
Erik Kimsey <kimseyerik@gmail.com>
2019-03-11 10:24:26 -0400

    Cleaning up Landing.js

--- src/Landing/Landing.js
+++ src/Landing/Landing.js
@@ -14,17 +14,14 @@ class Landing extends Component {
   render(){
     return (
       <div className="landing-container">
-      <div className="something-cool-container"></div>
-              <div className="self-blurb-container">
+        <div className="something-cool-container"></div>
+        <div className="self-blurb-container">
           <p>I work creatively with technology.</p>
           <p>I focus on web-based and mobile-web technology, design, and content. </p>
           <p>That said, I'm interested in work involving development of creative technology in the realm of mobile-web/PWA, responsive web,and IoT.</p>
           <p>I like to think I'm at the cross-section of interactivity + aesthetic-appeal + innovation + good purpose.</p>
           <p>I love learning new languages and technology.  Especially, in order to put them to use in novel ways.</p>
-        </div>
-        {/* <div className="flesh-container">
-          <Unburzt width={this.state.width} height={this.state.height} />
-        </div> */}
+      </div>
       </div>
     )
   }
