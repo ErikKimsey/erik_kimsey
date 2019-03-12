599888c36d19e4986ad178240c1c02597f17ff9b
Erik Kimsey <kimseyerik@gmail.com>
2019-03-12 13:39:03 -0400

    Building out Work.js in order to accept data from data.js

--- src/Work/Work.js
+++ src/Work/Work.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import './work.scss';
-
+import { data } from './data';
+ 
 const Work = () => {
   return (
     <div className="work-container">
-      <p></p>
-      <h1>Work</h1>
+     
+      <div className="work-data-list-contaner">
+        {
+          data.map((e) => {
+            return (
+              <div key={e.name} className="data-item-container">
+                <div className="item-name"></div>
+                <div className="item-stack"></div>
+                <div className="item-description"></div>
+                <div className="item-url"></div>
+                <div className="item-github"></div>
+              </div>
+            )
+          } )
+        }
+      </div>
     </div>
   )
 }
