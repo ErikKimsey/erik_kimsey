2b929bed144d457d67fb0e0f6e227536c180b24d
Erik Kimsey <kimseyerik@gmail.com>
2019-03-12 16:09:22 -0400

    fleshing out data in into item elements in Work.js

--- src/Work/Work.js
+++ src/Work/Work.js
@@ -11,11 +11,11 @@ const Work = () => {
           data.map((e) => {
             return (
               <div key={e.name} className="data-item-container">
-                <div className="item-name"></div>
-                <div className="item-stack"></div>
+                <div className="item-name">{e.name}</div>
+                <div className="item-stack">{e.stack}</div>
                 <div className="item-description"></div>
-                <div className="item-url"></div>
-                <div className="item-github"></div>
+                <div className="item-url">{e.url}</div>
+                <div className="item-github">{e.github}</div>
               </div>
             )
           } )
