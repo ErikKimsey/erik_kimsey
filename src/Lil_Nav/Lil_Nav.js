82f9d91c29d08d3497752524976c7a05fbc059fc
Erik Kimsey <kimseyerik@gmail.com>
2019-03-21 12:28:28 -0400

    Working on animation of dropdown in lilnav

--- src/Lil_Nav/Lil_Nav.js
+++ src/Lil_Nav/Lil_Nav.js
@@ -4,10 +4,19 @@ import { navListData } from '../__data/data';
 
 
 export default class Lil_Nav extends Component {
+
+  handleBtnClick = () => {
+    console.log('clicked!!');
+    /**
+     * 1. toggle class with animation attached,
+     * 
+     */
+  }
+
   render() {
     return (
       <div className="lil-nav-container">
-        <div className="nav-btn">BTN</div>
+        <div className="nav-btn" onClick={this.handleBtnClick}>BTN</div>
         <div className="nav-list">
         {
           navListData.map((e)=>{
