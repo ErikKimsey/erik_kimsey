6e845cf9c4803e0aa993350eb21ad9b6b1c04c12
Erik Kimsey <kimseyerik@gmail.com>
2019-03-22 13:38:42 -0400

    created icon for dropdown menu in lil-nav

--- src/Lil_Nav/Lil_Nav.js
+++ src/Lil_Nav/Lil_Nav.js
@@ -6,13 +6,20 @@ export default class Lil_Nav extends Component {
 
   toggleMenu = () => {
     let menu = document.querySelector('.nav-list');
+    let menuItems = document.querySelectorAll('.list-item');
+    menuItems.forEach((e)=>{
+      console.log(e);
+      if(   )
+    })
     menu.classList.toggle('no-show');
   }
 
   render() {
     return (
       <div className="lil-nav-container">
-        <div className="nav-btn" onClick={this.toggleMenu}>BTN</div>
+        <div className="lil-nav-btn" onClick={this.toggleMenu}>
+          <img src={require('../assets/icons/lil_nav_btn-8.png')} />
+        </div>
         <div className="nav-list">
         {
           this.props.data.map((e)=>{
