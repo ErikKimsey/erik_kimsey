786262a3e0b0152f595c5d16b134b922a87f9b08
Erik Kimsey <kimseyerik@gmail.com>
2019-03-28 11:01:33 -0400

    Removed phone number from Contact.  Added assets for favicon and other icons.  Replaced favicon.

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -14,7 +14,6 @@ class Contact extends Component {
         <div className="name contact-item">{CONTACT_DATA.name}</div>
         <div className="title contact-item">{CONTACT_DATA.role}</div>
         <div className="email-address contact-item" id="email-address">{CONTACT_DATA.email}</div>
-        <div className="phone-number contact-item">{CONTACT_DATA.phone}</div>
       </div>
     )
   }
