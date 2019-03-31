e3b8bdb8f163a01d62eed64a765f754473e16406
Erik Kimsey <kimseyerik@gmail.com>
2019-03-31 15:59:20 -0400

    Preliminary orientation detection and experimental style changes

--- src/App.js
+++ src/App.js
@@ -16,6 +16,26 @@ class App extends Component {
       toggled: false,
     }
   }
+  
+  componentDidMount() {
+
+    let orntn = window.screen.msOrientation || window.screen.mozOrientation || (window.screen.orientation || {}).type;
+    if(orntn === "landscape-primary"){
+      document.querySelector('.App').style.background = "rgb(211,88,199)";
+    } else {
+      document.querySelector('.App').style.background = "none";
+    }
+
+    window.addEventListener('orientationchange', ()=>{
+      let orntn = window.screen.msOrientation || window.screen.mozOrientation || (window.screen.orientation || {}).type;
+      console.log(orntn);
+      if(orntn === "landscape-primary"){
+        document.querySelector('.App').style.background = "rgb(211,88,199)";
+      } else {
+        document.querySelector('.App').style.background = "rgb(0,0,0)";
+      }
+    })
+  }
 
   handleToggle = () => {
     this.setState({toggle: !this.state.toggled});
