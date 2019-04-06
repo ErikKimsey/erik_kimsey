818087191d5375a150855b85dd6e5902743eae56
Erik Kimsey <kimseyerik@gmail.com>
2019-04-06 19:05:29 -0400

    I fudged my site up, now I'm fixing it.  CSS is dangerous.

--- src/Banner/Banner.js
+++ src/Banner/Banner.js
@@ -29,13 +29,15 @@ class Banner extends Component {
     return (
       <div className="banner-container">
         <div className="toggle-logo-container">
-            <span className="navbar-toggle" id="js-navbar-toggle" onClick={this.handleToggle} >
+          <span className="navbar-toggle" id="js-navbar-toggle" onClick={this.handleToggle} >
             <Spin className="spin" active={this.props.isActive} color="white" />
           </span>
         </div>
-        <div className="name banner-item">erik kimsey</div>
-        <div className="underscore banner-item">  ___ </div>
-        <div className="role banner-item">software developer</div>
+        <div className="text-container">
+          <div className="name banner-item">erik kimsey</div>
+          <div className="underscore banner-item">  ___ </div>
+          <div className="role banner-item">software developer</div>
+        </div>
       </div>
     )
   }
