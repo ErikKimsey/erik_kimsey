efb61259f3661ade0d075f48fd203ec27a7d887b
Erik Kimsey <kimseyerik@gmail.com>
2019-04-13 16:32:02 -0400

    Created Playground compoent. Import playground scss file. Imported globals and responsive.scss into playground.scss

---
+++ src/Playground/Playground.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { Route, Link } from 'react-router-dom';
+import './playground.scss';
+
+export default class Playground extends Component {
+  render() {
+    return (
+      <div className="playground-container">
+        
+      </div>
+    )
+  }
+}
