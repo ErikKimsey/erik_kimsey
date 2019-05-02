828cb0866a198ef45f3558a86a51d5936c05f826
Erik Kimsey <kimseyerik@gmail.com>
2019-05-02 09:47:56 -0400

    Getting weather data from openweather

--- src/Playground/Playground.js
+++ src/Playground/Playground.js
@@ -1,13 +1,40 @@
 import React, { Component } from 'react';
 import { Route, Link } from 'react-router-dom';
+import axios from 'axios';
 import './playground.scss';
 
+const HBURG_COORDS = {
+  lat: 38.44957,
+  long:  -78.86892
+}
+
+const HBURG_ID = 4763237;
+
+const WEATHER_URL = `http://api.openweathermap.org/data/2.5/forecast?id=4763237&APPID=48d7c5c77e90fd49d936f28a020abadc`;
+
+
 export default class Playground extends Component {
+
+  componentDidMount() {
+    axios.get(WEATHER_URL)
+    .then((res)=>{
+      console.log(res.data);
+    })
+    .catch((err)=>{
+      console.log(err);
+    })
+  }
+  
+
+  drawGraph = () => {
+    // return 
+  }
+
   render() {
     return (
-      <div className="playground-container">
+      <svg className="playground-container">
         
-      </div>
+      </svg>
     )
   }
 }
