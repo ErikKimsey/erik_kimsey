1777a026a58b99fc96d4e2783ef980857e2c86da
Erik Kimsey <kimseyerik@gmail.com>
2019-05-03 18:30:14 -0400

    So so much has been done.  But just completed hierarchization of data

--- src/Playground/playings/play.js
+++ src/Playground/playings/play.js
@@ -4,23 +4,30 @@ import * as d3 from "d3";
 export const draw = (data, container, clientDim) => {
   let w = clientDim.w, h = clientDim.h;
   
-  let body = d3.select(container).append('svg')
-    .attr('w', w)
-    .attr('h', h)
-    // .attr(back);
-  console.log(data[0]);
-  
+  if(data.length != 0) {
+    // let body = d3.select(container).append('svg')
+    //   .attr('w', w)
+    //   .attr('h', h)
+    //   .style('fill', 'pink');
+    
+    let layout = d3.pack().size([w,h]);
 
-  if(data != undefined) {
-    body.selectAll('g')
-      .data(data)
-      .enter()
-      .append('g')
-      .attr('className','data-point')
-      .text((d) => {
-        console.log(d);  
-        return d.main.temp;      
+      let root = {children: data.slice()};
+      let nodes = d3.hierarchy(root).sum((d) => { 
+        return d; 
       });
+    console.log(nodes);
+    
+    // body.selectAll('circle')
+    //   .data(data)
+    //   .enter()
+    //   .append('circle')
+    //   .attr('className','data-point')
+    //   .style('fill', 'pink')
+    //   .text((d) => {
+    //     // console.log(d);  
+    //     return d.main.temp;      
+    //   });
   }
   
 
