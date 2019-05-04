c7be6e617a2a0538e859918fd88be2bddc202908
Erik Kimsey <kimseyerik@gmail.com>
2019-05-04 14:44:44 -0400

    Creating nested data for play with D3.  Simple DAW

---
+++ src/Playground/data.js
@@ -0,0 +1,60 @@
+
+
+const PLAYGROUND_DATA = [
+  {
+    label: 'controls',
+    children: [
+      {
+        label: 'volume',
+        children: [
+          {
+            label:'increase',
+            value: 1
+          },
+          {
+            label:'decrease',
+            value: 1
+          },
+          {
+            label:'mute',
+            value: 0
+          }
+        ]
+      },
+      {
+        label: 'playback',
+        children: [
+          {
+            label:'play',
+            value: 'play'
+          },
+          {
+            label:'stop',
+            value: 'stop'
+          },
+          {
+            label:'pause',
+            value: 'pause'
+          },
+        ]
+      },
+      {
+        label: 'semitones',
+        children: [
+          {
+            label:'play',
+            value: 'play'
+          },
+          {
+            label:'stop',
+            value: 'stop'
+          },
+          {
+            label:'pause',
+            value: 'pause'
+          },
+        ]
+      },
+    ]
+  },
+]
