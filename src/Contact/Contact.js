dc41e558d1d6047422c8ab915454ede96304fbd5
Erik Kimsey <kimseyerik@gmail.com>
2019-05-08 10:04:37 -0400

    Restyling contact

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -1,22 +1,28 @@
 import React, { Component } from 'react';
 import './contact.scss';
 import { CONTACT_DATA } from '../__data/data';
+import droopy_e from '../assets/droopy_e.png';
 
+console.log(droopy_e);
 
 class Contact extends Component {
-  constructor(props){
-    super(props);
-  }
+	constructor(props) {
+		super(props);
+	}
 
-  render(){
-    return (
-      <div className="contact-container">
-        {/* <div className="name contact-item">{CONTACT_DATA.name}</div>
-        <div className="title contact-item">{CONTACT_DATA.role}</div> */}
-        <div className="email-address contact-item" id="email-address">{CONTACT_DATA.email}</div>
-      </div>
-    )
-  }
+	render() {
+		return (
+			<div className="contact-container">
+				<div className="email-address contact-item" id="email-address">
+					{CONTACT_DATA.email_1}
+					<span className="email-address contact-item">[@]</span>
+				</div>
+				<div className="email-address contact-item" id="email-address">
+					gmail<span>[dot]</span>com
+				</div>
+			</div>
+		);
+	}
 }
 
 export default Contact;
