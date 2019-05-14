9c9bb432fc8121268730b75770bd90fc7d3f62dc
Erik Kimsey <kimseyerik@gmail.com>
2019-05-14 07:24:02 -0400

    Changed lgDevice size in breakpoints

--- src/__styles/breakPoints.js
+++ src/__styles/breakPoints.js
@@ -1,9 +1,7 @@
-
-
 export const BREAKPOINTS = {
-  xsDevice:300,
-  smDevice:405,
-  mdDevice:700,
-  lgDevice:1024,
-  xlDevice:1250,
-}
+	xsDevice: 300,
+	smDevice: 405,
+	mdDevice: 700,
+	lgDevice: 1042,
+	xlDevice: 1250
+};
