5fc8cd4af86583fa8b9086a0cd48ef9cd409a92c
Erik Kimsey <kimseyerik@gmail.com>
2019-06-05 12:20:01 -0400

    Added img url and about keys for Work data

--- src/Work/data.js
+++ src/Work/data.js
@@ -32,43 +32,57 @@ export const data = [
 		name: 'Slide right >> ',
 		stack: 'To see some of my work!',
 		url: '',
-		github: ''
+		github: '',
+		img: '',
+		about: ''
+	},
+	{
+		name: 'Sveza',
+		stack: 'ReactJS, Apollo, GraphQL, Styled-Components',
+		url: 'https://team-home.netlify.com/',
+		github: 'https://github.com/Lambda-School-Labs/labs-team-home',
+		img: '',
+		about: ''
 	},
 	{
 		name: 'Interactive Guilloche',
 		stack: 'Angular v4, D3.js',
 		url: 'https://www.npmjs.com/package/nls-guilloche',
-		github: ''
+		github: '',
+		img: '',
+		about: ''
 	},
 	{
 		name: 'Conway\'s "Game of Life"',
 		stack: 'P5.js, ReactJS',
 		url: 'https://wizardly-lewin-048675.netlify.com/',
-		github: 'https://github.com/ErikKimsey/Conways-Life'
+		github: 'https://github.com/ErikKimsey/Conways-Life',
+		img: '',
+		about: ''
 	},
 	{
 		name: 'Audio Synthesizer',
 		stack: 'Web Audio API, JavaScript, (some weird usage of) CSS',
 		url: '',
-		github: 'https://github.com/ErikKimsey/Simple_Synthesizer_usingWebAudioAPI'
+		github: 'https://github.com/ErikKimsey/Simple_Synthesizer_usingWebAudioAPI',
+		img: '',
+		about: ''
 	},
 	{
 		name: 'Mobile Motion Painter',
 		stack: 'Gyroscope & Accelerometer of the W3C Sensor API, P5.js',
 		url: '',
-		github: 'https://github.com/ErikKimsey/MobileMotionPaint'
-	},
-	{
-		name: 'Sveza - Lambda School Capstone Project',
-		stack: 'ReactJS, Apollo, GraphQL, Styled-Components',
-		url: 'https://team-home.netlify.com/',
-		github: 'https://github.com/Lambda-School-Labs/labs-team-home'
+		github: 'https://github.com/ErikKimsey/MobileMotionPaint',
+		img: '',
+		about: ''
 	},
 	{
 		name: 'HTTP Server (C lang)',
 		stack: 'C Language',
 		url: '',
-		github: 'https://github.com/ErikKimsey/C-Web-Server/tree/master/src'
+		github: 'https://github.com/ErikKimsey/C-Web-Server/tree/master/src',
+		img: '',
+		about: ''
 	},
 	// {
 	// 	name: 'Roman-Numeral Calculator ',
@@ -80,6 +94,8 @@ export const data = [
 		name: '8-bit CPU Emulator',
 		stack: 'C Language',
 		url: '',
-		github: 'https://github.com/ErikKimsey/Computer-Architecture/tree/master/ls8'
+		github: 'https://github.com/ErikKimsey/Computer-Architecture/tree/master/ls8',
+		img: '',
+		about: ''
 	}
 ];
