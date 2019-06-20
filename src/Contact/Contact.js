18bb148656b420eb99f53234740e2f334b6c48d3
Erik Kimsey <kimseyerik@gmail.com>
2019-06-20 10:40:38 -0400

    divideString function working to split string into array of chars

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -3,10 +3,18 @@ import './contact.scss';
 import { CONTACT_DATA } from '../__data/data';
 
 const Contact = () => {
+	const divideString = () => {
+		return CONTACT_DATA.email_1.split('').map((e, i) => {
+			console.log(e);
+			return <div className="letter">{e}</div>;
+		});
+	};
+	console.log(divideString());
+
 	return (
 		<div className="contact-container">
 			<div className="email-address contact-item" id="email-address">
-				{CONTACT_DATA.email_1}
+				{divideString()}
 				<span className="email-address contact-item">[@]</span>
 			</div>
 			<div className="email-address contact-item" id="email-address">
