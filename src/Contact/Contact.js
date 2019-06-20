08976ff8d5dff274991b42a9398976104620a93f
Erik Kimsey <kimseyerik@gmail.com>
2019-06-20 10:47:09 -0400

    Pushing contact strings through divideString

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -1,24 +1,23 @@
 import React from 'react';
 import './contact.scss';
 import { CONTACT_DATA } from '../__data/data';
+const atGmail = 'gmail.com';
 
 const Contact = () => {
-	const divideString = () => {
-		return CONTACT_DATA.email_1.split('').map((e, i) => {
-			console.log(e);
+	const divideString = (str) => {
+		return str.split('').map((e, i) => {
 			return <div className="letter">{e}</div>;
 		});
 	};
-	console.log(divideString());
 
 	return (
 		<div className="contact-container">
 			<div className="email-address contact-item" id="email-address">
-				{divideString()}
-				<span className="email-address contact-item">[@]</span>
+				{divideString(CONTACT_DATA.email_1)}
+				<span className="email-address contact-item letter">[@]</span>
 			</div>
 			<div className="email-address contact-item" id="email-address">
-				gmail<span>[dot]</span>com
+				{divideString(atGmail)}
 			</div>
 		</div>
 	);
