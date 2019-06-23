30a7e96e9de44954c124aee4c5e0ca7dd90442c7
Erik Kimsey <kimseyerik@gmail.com>
2019-06-23 11:27:12 -0400

    Added JS-based sendEmail functionality in Contact

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -2,15 +2,25 @@ import React from 'react';
 import './contact.scss';
 import { CONTACT_DATA } from '../__data/data';
 
+const EMAIL = {
+	prefix: 'kimseyerik',
+	suffix: 'gmail.com'
+};
+
+// window.open( String( 'mailto:recipient^example.com' ).replace('^', '@') );
 const Contact = () => {
+	const sendEmail = () => {
+		console.log('sending');
+		window.open(String(`mailto:${EMAIL.prefix}^${EMAIL.suffix}`).replace('^', '@'));
+	};
 	return (
 		<div className="contact-container">
-			<div className="email-address contact-item" id="email-address">
-				{CONTACT_DATA.email_1}
-				<span className="email-address contact-item">[at]</span>
+			<div className="email-address contact-item" id="email-address" onClick={sendEmail}>
+				{EMAIL.prefix}
+				<span className="email-address contact-item">@</span>
 			</div>
-			<div className="email-address contact-item" id="email-address">
-				gmail<span>[dot]</span>com
+			<div className="email-address contact-item" id="email-address" onClick={sendEmail}>
+				gmail.com
 			</div>
 		</div>
 	);
