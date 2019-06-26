7ef41126dbdb42e345ddefa4d47ef08f6352e376
Erik Kimsey <kimseyerik@gmail.com>
2019-06-26 11:58:19 -0400

    Changes to Employment.js

--- src/About/components/Employment.js
+++ src/About/components/Employment.js
@@ -2,32 +2,32 @@ import React from 'react';
 import './styles/employment.scss';
 
 export default function Employment(props) {
-  return (
-    <div className="employment-container" id="EXPERIENCE">
-      <div className="employer-name">{props.data.employer}</div>
-      <div className="role">{props.data.role}</div>
-      <div className="duration">{props.data.duration}</div>
-      <div className="role-description">{props.data.description}</div>
-      <div className="projects-container">
-        <ul>
-          {
-            props.data.projects.map((e,i)=>{
-              return <li key={e}>{e}</li>
-            })
-          }
-        </ul>
-      </div>
-      <div className="tech-container">
-        <ul>
-          {
-            props.data.tech.map((e,i)=>{
-              return <li key={e}>{e}</li>
-            })
-          }
-        </ul>
-      </div>
-      <div className=""></div>
-      <div className=""></div>
-    </div>
-  )
+	return (
+		<div className="employment-container" id="EXPERIENCE">
+			<div className="employer-name">{props.data.employer}</div>
+			<div className="role">{props.data.role}</div>
+			<div className="duration">{props.data.duration}</div>
+			{/* Add links to products in place of role description */}
+			{/* <div className="role-description bold-list-title">{props.data.description}</div> */}
+			<div className="projects-container">
+				<br />
+				<b className="bold-list-title">Specific Involvement:</b>
+				<ul>
+					{props.data.projects.map((e, i) => {
+						return <li key={e}>{e}</li>;
+					})}
+				</ul>
+			</div>
+			<div className="tech-container">
+				<b>Technologies:</b>{' '}
+				<ul>
+					{props.data.tech.map((e, i) => {
+						return <li key={e}>{e}</li>;
+					})}
+				</ul>
+			</div>
+			<div className="" />
+			<div className="" />
+		</div>
+	);
 }
