71eecc92e6c2439b9bd7b1be1c1124e20ade5598
Erik Kimsey <kimseyerik@gmail.com>
2019-06-26 16:23:52 -0400

    Removed fontawesome icon for resume icon. Replaced with screenshot of new resume

--- src/About/components/Resume.js
+++ src/About/components/Resume.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './styles/resume.scss';
 import resumePDF from '../../assets/resume/erikkimsey_software_engineer_resume.pdf';
+import resumeShot from '../../assets/resume/resume_shot_small.png';
 
 export default function Resume() {
 	return (
@@ -10,7 +11,8 @@ export default function Resume() {
 			</div>
 			<div className="image-container resume-sub-container">
 				<a href={resumePDF} target="_blank" rel="noopener noreferrer">
-					<i className="far fa-file-pdf fa-10x" />
+					{/* <i className="far fa-file-pdf fa-10x" /> */}
+					<img src={resumeShot} />
 				</a>
 			</div>
 		</div>
