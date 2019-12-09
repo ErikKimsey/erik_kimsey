9b97cfddc135f8f068e07fc4244ab65ca08f4ab6
Erik Kimsey <kimseyerik@gmail.com>
2019-12-09 10:02:50 -0500

    Updated tech skills and work history

--- src/Work/data.js
+++ src/Work/data.js
@@ -28,106 +28,124 @@
  */
 
 export const data = [
-	{
-		name: 'My Portfolio',
-		stack: 'Slide right.',
-		url: '',
-		github: '',
-		img: '',
-		about: ''
-	},
-	{
-		name: 'Connect Our Kids',
-		stack: 'React-Native, Redux, Auth0, Swagger',
-		url: 'https://www.connectourkids.org/',
-		github: '',
-		img: '',
-		about: 'Contributed to planning and architecture of Connect Our Kids "Family Connections" in their React-Native application'
-	},
-	{
-		name: 'Sveza',
-		stack: 'ReactJS, Apollo, GraphQL, Styled-Components',
-		url: 'https://team-home.netlify.com/',
-		github: 'https://github.com/Lambda-School-Labs/labs-team-home',
-		img: '../assets/port_images/sveza.png',
-		about: ''
-	},
-	{
-		name: 'Raincrow Studios - Covens',
-		stack: 'JavaScript (ES6), NodeJS, Redis, Python / Pandas',
-		url: 'https://play.google.com/store/apps/details?id=com.raincrow.covens&hl=en_US',
-		github: '',
-		img: '../assets/port_images/raincrow.png',
-		about: ''
-	},
-	{
-		name: 'ChartIQ',
-		stack: 'JavaScript, Angular, HTML5 / Canvas API, CSS3',
-		url: 'https://www.chartiq.com/html5-charting-library/',
-		github: '',
-		img: '../assets/port_images/chartiq.png',
-		about: ''
-	},
-	{
-		name: 'NextLevel - Interactive Guilloche',
-		stack: 'Angular v4, D3.js',
-		url: 'https://www.npmjs.com/package/nls-guilloche',
-		github: '',
-		img: '',
-		about: ''
-	},
-	{
-		name: 'Kardion',
-		stack: 'Boostrap 4, Sass',
-		url: 'http://www.kardion.de/en/imprint',
-		github: '',
-		img: '',
-		about: ''
-	},
-	{
-		name: 'Conway\'s "Game of Life"',
-		stack: 'P5.js, ReactJS',
-		url: 'https://wizardly-lewin-048675.netlify.com/',
-		github: 'https://github.com/ErikKimsey/Conways-Life',
-		img: '',
-		about: ''
-	},
-	// {
-	// 	name: 'Audio Synthesizer',
-	// 	stack: 'Web Audio API, JavaScript, (some weird usage of) CSS',
-	// 	url: '',
-	// 	github: 'https://github.com/ErikKimsey/Simple_Synthesizer_usingWebAudioAPI',
-	// 	img: '',
-	// 	about: ''
-	// },
-	// {
-	// 	name: 'Mobile Motion Painter',
-	// 	stack: 'Gyroscope & Accelerometer of the W3C Sensor API, P5.js',
-	// 	url: '',
-	// 	github: 'https://github.com/ErikKimsey/MobileMotionPaint',
-	// 	img: '',
-	// 	about: ''
-	// },
-	{
-		name: 'HTTP Server',
-		stack: 'C Language',
-		url: '',
-		github: 'https://github.com/ErikKimsey/C-Web-Server/tree/master/src',
-		img: '',
-		about: ''
-	},
-	// {
-	// 	name: 'Roman-Numeral Calculator ',
-	// 	stack: 'NodeJS, HTML, JavaScript',
-	// 	url: 'https://young-bayou-82655.herokuapp.com/',
-	// 	github: 'https://github.com/ErikKimsey/Roman_Numeral_Calculator'
-	// },
-	{
-		name: '8-bit CPU Emulator',
-		stack: 'C Language',
-		url: '',
-		github: 'https://github.com/ErikKimsey/Computer-Architecture/tree/master/ls8',
-		img: '',
-		about: ''
-	}
+  {
+    name: 'My Portfolio',
+    stack: 'Slide right.',
+    url: '',
+    github: '',
+    img: '',
+    about: ''
+  },
+  {
+    name: 'Chatwise',
+    stack: 'PostGres, Express / NodeJS, React, Sass, JavaScript / ES6, Messenger API',
+    url: '',
+    github: '',
+    img: '',
+    about:
+      'Using Messenger API, Postgres, and React, develop a dynamic chatbot that provides Blinkist-type functionality within Messenger.'
+  },
+  {
+    name: 'Realm',
+    stack: 'React-Native,Redux, Styled-Components, JavaScript / ES6, Expo, Figma',
+    url: 'https://www.therealm.gg/',
+    github: '',
+    img: '',
+    about: 'Within React-Native, I contributed to architecting and developing the Realm gamer-focused social-media app.'
+  },
+  {
+    name: 'Connect Our Kids',
+    stack: 'React-Native, Redux, Auth0, Swagger',
+    url: 'https://www.connectourkids.org/',
+    github: '',
+    img: '',
+    about:
+      'Contributed to planning and architecture of Connect Our Kids "Family Connections" in their React-Native application'
+  },
+  {
+    name: 'Sveza',
+    stack: 'ReactJS, Apollo, GraphQL, Styled-Components',
+    url: 'https://team-home.netlify.com/',
+    github: 'https://github.com/Lambda-School-Labs/labs-team-home',
+    img: '../assets/port_images/sveza.png',
+    about: ''
+  },
+  {
+    name: 'Raincrow Studios - Covens',
+    stack: 'JavaScript (ES6), NodeJS, Redis, Python / Pandas',
+    url: 'https://play.google.com/store/apps/details?id=com.raincrow.covens&hl=en_US',
+    github: '',
+    img: '../assets/port_images/raincrow.png',
+    about: ''
+  },
+  {
+    name: 'ChartIQ',
+    stack: 'JavaScript, Angular, HTML5 / Canvas API, CSS3',
+    url: 'https://www.chartiq.com/html5-charting-library/',
+    github: '',
+    img: '../assets/port_images/chartiq.png',
+    about: ''
+  },
+  {
+    name: 'NextLevel - Interactive Guilloche',
+    stack: 'Angular v4, D3.js',
+    url: 'https://www.npmjs.com/package/nls-guilloche',
+    github: '',
+    img: '',
+    about: ''
+  },
+  {
+    name: 'Kardion',
+    stack: 'Boostrap 4, Sass',
+    url: 'http://www.kardion.de/en/imprint',
+    github: '',
+    img: '',
+    about: ''
+  },
+  {
+    name: 'Conway\'s "Game of Life"',
+    stack: 'P5.js, ReactJS',
+    url: 'https://wizardly-lewin-048675.netlify.com/',
+    github: 'https://github.com/ErikKimsey/Conways-Life',
+    img: '',
+    about: ''
+  },
+  // {
+  // 	name: 'Audio Synthesizer',
+  // 	stack: 'Web Audio API, JavaScript, (some weird usage of) CSS',
+  // 	url: '',
+  // 	github: 'https://github.com/ErikKimsey/Simple_Synthesizer_usingWebAudioAPI',
+  // 	img: '',
+  // 	about: ''
+  // },
+  // {
+  // 	name: 'Mobile Motion Painter',
+  // 	stack: 'Gyroscope & Accelerometer of the W3C Sensor API, P5.js',
+  // 	url: '',
+  // 	github: 'https://github.com/ErikKimsey/MobileMotionPaint',
+  // 	img: '',
+  // 	about: ''
+  // },
+  {
+    name: 'HTTP Server',
+    stack: 'C Language',
+    url: '',
+    github: 'https://github.com/ErikKimsey/C-Web-Server/tree/master/src',
+    img: '',
+    about: ''
+  },
+  // {
+  // 	name: 'Roman-Numeral Calculator ',
+  // 	stack: 'NodeJS, HTML, JavaScript',
+  // 	url: 'https://young-bayou-82655.herokuapp.com/',
+  // 	github: 'https://github.com/ErikKimsey/Roman_Numeral_Calculator'
+  // },
+  {
+    name: '8-bit CPU Emulator',
+    stack: 'C Language',
+    url: '',
+    github: 'https://github.com/ErikKimsey/Computer-Architecture/tree/master/ls8',
+    img: '',
+    about: ''
+  }
 ];
