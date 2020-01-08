e1ba5860b9f8ad70f64d916fb45d54eba3dfea79
Erik Kimsey <kimseyerik@gmail.com>
2020-01-08 10:14:33 -0500

    grammar changes in landing

--- src/Landing/data.js
+++ src/Landing/data.js
@@ -1,5 +1,5 @@
 export const data = [
-  'High-Technology is a artistic medium. It is no less organic to creativity and culture than the paint brush, the violin, or pen.',
+  'High-Technology is an artistic medium. It is no less organic to creativity and culture than the paint brush, the violin, or pen.',
   "Hey, there! I'm Erik.",
   'I am at the cross-section of interactivity, aesthetic, innovation, and good purpose with technology.'
 ];
