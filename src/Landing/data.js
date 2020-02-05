575c73cc693cf4933e19ff34ad8170e2da95ec73
Erik Kimsey <kimseyerik@gmail.com>
2020-02-05 10:07:09 -0500

    adjusted landing blurb font size

--- src/Landing/data.js
+++ src/Landing/data.js
@@ -1,4 +1,4 @@
 export const data = [
-	'My focus is building dynamic, interactive UI/UX...with whatever technology is required.',
+	'My focus is building dynamic, interactive UI/UX \n...with whatever technology is required.',
 	'I am at the cross-section of interactivity, aesthetic, technological innovation, and good purpose.'
 ];
