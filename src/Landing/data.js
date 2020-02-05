8e3abd017c616df083af4f29463c05333ff6c367
Erik Kimsey <kimseyerik@gmail.com>
2020-02-05 09:48:30 -0500

    Rewrote sm banner

--- src/Landing/data.js
+++ src/Landing/data.js
@@ -1,4 +1,4 @@
 export const data = [
 	'My focus is building dynamic, interactive UI/UX...with whatever technology is required.',
-	'I am at the cross-section of interactivity, aesthetic, innovation, and good purpose with technology.'
+	'I am at the cross-section of interactivity, aesthetic, technological innovation, and good purpose.'
 ];
