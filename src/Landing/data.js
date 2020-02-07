78c3f1b7b69477a156142fa4184ee793d3086054
Erik Kimsey <kimseyerik@gmail.com>
2020-02-07 09:50:43 -0500

    Changed landing banner

--- src/Landing/data.js
+++ src/Landing/data.js
@@ -1,4 +1,4 @@
 export const data = [
-	'My focus is building dynamic, interactive UI/UX \n...with whatever technology is required.',
+	'I build dynamic, beautiful, and interactive technology.',
 	'I am at the cross-section of interactivity, aesthetic, technological innovation, and good purpose.'
 ];
