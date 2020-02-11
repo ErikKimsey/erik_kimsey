f69b0b2a94ccb420d6da1b47fab5e57e4ce070ed
Erik Kimsey <kimseyerik@gmail.com>
2020-02-11 09:46:42 -0500

    Changes to landing banner. Updated resume.

--- src/Landing/data.js
+++ src/Landing/data.js
@@ -1,4 +1,5 @@
 export const data = [
-	'My focus is building dynamic, interactive UI/UX \n...with whatever technology is required.',
-	'I am at the cross-section of interactivity, aesthetic, technological innovation, and good purpose.'
+	'My passion is pairing my creative and aesthetic inclinations with a boundless technical skill-set, to create truly memorable experiences.',
+	'I build dynamic, interactive, and responsive UI/UX for mobile and mobile-web.',
+	'Mobile-Web/ \nJavaScript (ES6+)/, \nReactJS/, \n\nReact-Native/, \n\nNodeJS/, \nand devouring... \n\nUnity3D and Blender.'
 ];
