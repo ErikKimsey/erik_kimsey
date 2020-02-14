e2842977255082e9b24d0593f227c2c97855d63d
Erik Kimsey <kimseyerik@gmail.com>
2020-02-14 10:25:54 -0500

    Starting threejs on landing

--- src/Landing/Landing.js
+++ src/Landing/Landing.js
@@ -1,33 +1,35 @@
 import React, { Component } from 'react';
+import ThreeScene from './ThreeScene/ThreeScene';
 import './landing.scss';
 import { data } from './data';
 import IconLinks from '../IconLinks/IconLinks';
 
 class Landing extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      width: 0,
-      height: 0
-    };
-    this.landingBlurbs = React.createRef();
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			width: 0,
+			height: 0
+		};
+		this.landingBlurbs = React.createRef();
+	}
 
-  render() {
-    return (
-      <div className="landing-container" ref={(e) => (this.landingBlurbs = e)}>
-        {data.map((e, i) => {
-          i += 1;
-          return (
-            <div className={`self-blurb-container blurb-${i}`} key={e}>
-              {e}
-            </div>
-          );
-        })}
-        <IconLinks />
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="landing-container" ref={(e) => (this.landingBlurbs = e)}>
+				<ThreeScene />
+				{data.map((e, i) => {
+					i += 1;
+					return (
+						<div className={`self-blurb-container blurb-${i}`} key={e}>
+							{e}
+						</div>
+					);
+				})}
+				<IconLinks />
+			</div>
+		);
+	}
 }
 
 export default Landing;
