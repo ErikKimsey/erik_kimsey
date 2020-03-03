cbe2979023ac15e878917d754608a78a62daa120
Erik Kimsey <kimseyerik@gmail.com>
2020-03-03 11:58:17 -0500

    Changes to landing header

--- src/Landing/data.js
+++ src/Landing/data.js
@@ -1,5 +1,5 @@
 export const data = [
-	'My passion is pairing my creative and aesthetic inclinations with a boundless technical skill-set, to create truly memorable experiences.',
-	'I build dynamic, interactive, and responsive UI/UX for mobile and mobile-web.',
-	'Mobile-Web/ \nJavaScript (ES6+)/, \nReactJS/, \n\nReact-Native/, \n\nNodeJS/, \nand devouring... \n\nUnity3D and Blender.'
+	"Passionate about building dynamic, interactive, intuitive and beautiful UI's/UX's for mobile & web. Always thirsty to learn new technology.",
+	'Polyglot. Polymath. \n A Creative. \n Artist. And pretty cool dude.',
+	'JavaScript (ES6+) /, \nReactJS /, \n\nReact-Native /, \n\nNodeJS/, \n\nC# & Unity3D /, Blender /, and   \n\n Adobe CC /.'
 ];
