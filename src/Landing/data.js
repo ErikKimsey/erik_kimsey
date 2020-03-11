91e3f31e0662d177656c0147c3c8320669cdcb9c
Erik Kimsey <kimseyerik@gmail.com>
2020-03-11 11:51:49 -0400

    Uncommented background bush

--- src/Landing/data.js
+++ src/Landing/data.js
@@ -1,5 +1,5 @@
 export const data = [
 	"Passionate about building dynamic, interactive, intuitive and beautiful UI's/UX's for mobile & web. Always thirsty to learn new technology.",
 	'Polyglot. Polymath. \n A Creative. \n Artist. And pretty cool dude.',
-	'JavaScript (ES6+) /, \nReactJS /, \n\nReact-Native /, \n\nNodeJS/, \n\nC# & Unity3D /, Blender /, and   \n\n Adobe CC /.'
+	'JavaScript (ES6+). \nReactJS. \n\nReact-Native. \n\nNodeJS., \n\nC# & Unity3D. Blender. \n\n Adobe CC.'
 ];
