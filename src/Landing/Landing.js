1c6d09cc8ea7f91b4e2ae9dcc96228c254bbabb8
Erik <kimseyerik@gmail.com>
2021-04-30 09:17:30 -0400

    Added core skills list

--- src/Landing/Landing.js
+++ src/Landing/Landing.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './landing.scss';
-import { data } from './data';
+import { data, coreSkills } from './data';
 import IconLinks from '../IconLinks/IconLinks';
 
 class Landing extends Component {
@@ -17,6 +17,15 @@ class Landing extends Component {
 		return (
 			<div className="landing-container" ref={(e) => (this.landingBlurbs = e)}>
 				{data == null && <div className="null-data" />}
+                <div className="skill-list-container">
+                    <h2>core skillset</h2>
+                    {
+                        coreSkills && 
+                            coreSkills.map((e) => {
+                                return <li>{`${e}`}</li>
+                            })
+                    }
+                    </div>
 				{data &&
 					data.map((e, i) => {
 						i += 1;
--- src/Landing/data.js
+++ src/Landing/data.js
@@ -1,4 +1,11 @@
 export const data = [
 	"I (seek to) build dynamic, interactive, intuitive and beautiful UI's/UX's for mobile & web.",
-	'JavaScript (ES6+). ReactJS. React-Native.  NodeJS., C# & Unity3D. Blender. Adobe CC.'
+	// 'JavaScript (ES6+), ReactJS, React-Native,  NodeJS, C# / Unity3D, Blender, Adobe CC.'
+];
+
+export const coreSkills = [
+    'JavaScript (ES6+)',
+    'ReactJS',
+    'React-Native',
+    'NodeJS', 'C# / Unity3D', 'Blender', 'Adobe CC'
 ];
