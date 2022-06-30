5c9ca1c3c6c6dc6621aa3ecb67882585fc2b1d18
Erik <kimseyerik@gmail.com>
2022-06-30 10:02:02 -0400

    Made blurb text smaller

--- src/Landing/data.js
+++ src/Landing/data.js
@@ -1,6 +1,5 @@
 export const data = [
-	"Mountains can be moved. An endeavour that technology has no small part.  Be it in medicine, the environment, infrastructure, education, academic disciplines, journalism, mental health, communication, economic (or any) policy, entertainment, architecture or art.",
-	// 'JavaScript (ES6+), ReactJS, React-Native,  NodeJS, C# / Unity3D, Blender, Adobe CC.'
+	"Mountains can be moved. An endeavour in which technology has no small part, no small responsibility. Be it in medicine, the environment, infrastructure, education, academic disciplines, journalism, mental health, communication, economic (or any) policy, entertainment, architecture or art. Technology is, or should be, a veritable well-spring of miracles.",
 ];
 
 export const subData =
