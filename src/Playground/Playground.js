854ee1d5ad455328bf52e632378a0807d9c81e7f
Erik <kimseyerik@gmail.com>
2022-09-13 09:40:56 -0400

    Added drei.  Have Traveling displaying proper context

--- src/Playground/Playground.js
+++ src/Playground/Playground.js
@@ -1,18 +1,25 @@
-import React, { useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import "./playground.scss";
 import Traveling from '../ThreeSketches/Traveling';
 
 export default function Playground() {
 
+    let [divDimens, setDivDimens] = useState();
     const playgroundContentRef = useRef();
+
+    useEffect(() => {
+        let w = playgroundContentRef.current.offsetWidth;
+        let h = playgroundContentRef.current.offsetHeight;
+        setDivDimens({ w: w, h: h });
+    }, []);
 
     return (
         <div className="playgroundContainer" >
             <div className='playgroundHeader'>
                 <div className='label'>Playground</div>
             </div>
-            <div className='playgroundContent'>
-                <Traveling />
+            <div className='playgroundContent' ref={playgroundContentRef}>
+                {divDimens && <Traveling dimens={divDimens} />}
             </div>
         </div>
     );
--- src/ThreeSketches/Traveling.jsx
+++ src/ThreeSketches/Traveling.jsx
@@ -1,23 +1,37 @@
 import React, { useEffect, useState } from "react";
-import { Canvas } from '@react-three/fiber'
+import { Canvas } from '@react-three/fiber';
 import * as THREE from "three";
+import { OrbitControls, PerspectiveCamera } from "@react-three/drei";
 
-function Box() {
-    return <mesh position={new THREE.Vector3(0, -1, 4)} rotation={[2, 2, 2]}>
+
+function Box(props) {
+    let { position } = props;
+    return <mesh position={position} rotation={[2, 2, 2]}>
         <meshStandardMaterial color="#ff00ff" />
         <boxBufferGeometry />
     </mesh>
 }
 
 export default function Traveling(props) {
+
+    let { w, h } = props.dimens;
+    let [dim, setDim] = useState();
+
+    console.log(props.dimens);
+
     useEffect(() => {
-        console.log("Traveling");
+        setDim({ w: w, h: h });
     }, []);
 
 
-    return <Canvas style={{ background: "#f0f" }}>
-        <Box />
+    return <Canvas style={{ background: "#333", width: w, height: h }}>
+        <PerspectiveCamera />
+        <OrbitControls />
+        <Box position={new THREE.Vector3(0, -1, 0)} />
+        <Box position={new THREE.Vector3(0, 1, 0)} />
+        <Box position={new THREE.Vector3(-5, 1, 3)} />
+        <Box position={new THREE.Vector3(4, 1, -3)} />
         <ambientLight intensity={0.2} color="#fff" />
-        <directionalLight color="#ffffff" intensity={2} position={[0, 0, 10]} />
+        <directionalLight color="#ffffff" intensity={2} position={[0, 0, 5]} />
     </Canvas>
 }
